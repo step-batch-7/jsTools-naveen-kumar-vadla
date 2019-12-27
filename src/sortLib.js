3c16b6816c59477c6460819bbcb3ab0fefe3b4e6
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-27 13:12:45 +0530

    Modified functions
    What changes you have made?
    1. Modified functions
       -sortByFields
       -sortLines
    2. Added a function getRequiredField

--- src/sortLib.js
+++ src/sortLib.js
@@ -1,15 +1,19 @@
 "use strict";
 
+const getRequiredField = (line, delimiter, columnNumber) => {
+	return line.split(delimiter)[columnNumber];
+};
+
 const sortByFields = (delimiter, columnNumber, line1, line2) => {
-	const line1Field = line1.split(delimiter)[columnNumber];
-	const line2Field = line2.split(delimiter)[columnNumber];
+	if (columnNumber > line1.split(delimiter).length) columnNumber = 0;
+	let line1Field = getRequiredField(line1, delimiter, columnNumber);
+	let line2Field = getRequiredField(line2, delimiter, columnNumber);
 	if (line1Field < line2Field) return -1;
 	if (line1Field > line2Field) return 1;
 	return 0;
 };
 
 const sortLines = (lines, columnNumber, delimiter) => {
-	if (columnNumber > lines[0].split(delimiter).length) return lines.sort();
 	lines.sort(sortByFields.bind(null, delimiter, columnNumber - 1));
 	return lines;
 };
