3c22bf49dd40a6dfe7558380c169c0023f780a3c
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-29 12:03:45 +0530

    Modified main function

--- sort.js
+++ sort.js
@@ -6,8 +6,7 @@ const { stdout, stderr } = process;
 const { performSort } = require('./src/sortLib');
 
 const main = () => {
-  const two = 2;
-  const userArgs = process.argv.slice(two);
+  const [, , ...userArgs] = process.argv;
   const { sortedLines, error } = performSort(userArgs, fs);
   stdout.write(sortedLines);
   stderr.write(error);
