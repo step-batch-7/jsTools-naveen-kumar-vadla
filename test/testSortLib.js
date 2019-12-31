8731c4e89db79412a0047a7897faa19b8053a7cf
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2019-12-31 11:39:32 +0530

    Added tests for stdin

--- test/testSortLib.js
+++ test/testSortLib.js
@@ -143,13 +143,6 @@ describe('performSort', () => {
       fileReadStream.emit('error', { code: 'ENOENT' });
       assert.ok(onSortCompletion.calledWith({ sortedLines: '', error }));
     });
-    it('Should give error for invalid column number', () => {
-      const userArgs = ['-k', '-1', './docs/sampleFile.txt'];
-      const error = 'sort: -k -1: Invalid argument';
-      const onSortCompletion = sinon.fake();
-      performSort(userArgs, {}, onSortCompletion);
-      assert.ok(onSortCompletion.calledWith({ sortedLines: '', error }));
-    });
     it('Should give error message for directory as file', () => {
       const userArgs = ['-k', '1', './docs/'];
       const error = 'sort: Is a directory';
@@ -160,7 +153,7 @@ describe('performSort', () => {
       fileReadStream.emit('error', { code: 'EISDIR' });
       assert.ok(onSortCompletion.calledWith({ sortedLines: '', error }));
     });
-    it('Should give error message file w/out permissions', () => {
+    it('Should give error message for file w/out permissions', () => {
       const userArgs = ['-k', '1', './docs/sampleFile.txt'];
       const error = 'sort: Permission denied';
       const onSortCompletion = sinon.fake();
@@ -168,6 +161,54 @@ describe('performSort', () => {
       const createReadStream = sinon.fake.returns(fileReadStream);
       performSort(userArgs, { createReadStream }, onSortCompletion);
       fileReadStream.emit('error', { code: 'EACCES' });
+      assert.ok(onSortCompletion.calledWith({ sortedLines: '', error }));
+    });
+    it('Should give error for invalid column number', () => {
+      const userArgs = ['-k', '-1', './docs/sampleFile.txt'];
+      const error = 'sort: -k -1: Invalid argument';
+      const onSortCompletion = sinon.fake();
+      performSort(userArgs, {}, onSortCompletion);
+      assert.ok(onSortCompletion.calledWith({ sortedLines: '', error }));
+    });
+  });
+  describe('Operation on stdin', () => {
+    it('Should give sorted data of given stdin content', () => {
+      const userArgs = ['-k', '1', './docs/sampleFile.txt'];
+      const sortedLines = '1 i\n2 h\na 9\na b\nb 8\nb c';
+      const onSortCompletion = sinon.fake();
+      const stdin = new EventEmitter();
+      const createReadStream = sinon.fake.returns(stdin);
+      performSort(userArgs, { createReadStream }, onSortCompletion);
+      stdin.emit('data', 'a 9\n1 i\nb 8\n2 h\na b\nb c\n');
+      stdin.emit('end');
+      assert.ok(onSortCompletion.calledWith({ sortedLines, error: '' }));
+    });
+    it('Should give data sorted normally for absent field', () => {
+      const userArgs = ['-k', '1', './docs/sampleFile.txt'];
+      const sortedLines = 'g 2 w\nh 3 x\ni 4 y\nj 5 z';
+      const onSortCompletion = sinon.fake();
+      const stdin = new EventEmitter();
+      const createReadStream = sinon.fake.returns(stdin);
+      performSort(userArgs, { createReadStream }, onSortCompletion);
+      stdin.emit('data', 'j 5 z\ni 4 y\ng 2 w\nh 3 x');
+      stdin.emit('end');
+      assert.ok(onSortCompletion.calledWith({ sortedLines, error: '' }));
+    });
+    it('Should give empty string for empty data', () => {
+      const userArgs = ['-k', '1', './docs/sampleFile.txt'];
+      const onSortCompletion = sinon.fake();
+      const stdin = new EventEmitter();
+      const createReadStream = sinon.fake.returns(stdin);
+      performSort(userArgs, { createReadStream }, onSortCompletion);
+      stdin.emit('data', '');
+      stdin.emit('end');
+      assert.ok(onSortCompletion.calledWith({ sortedLines: '', error: '' }));
+    });
+    it('Should give error for invalid column number', () => {
+      const userArgs = ['-k', '-1', './docs/sampleFile.txt'];
+      const error = 'sort: -k -1: Invalid argument';
+      const onSortCompletion = sinon.fake();
+      performSort(userArgs, {}, onSortCompletion);
       assert.ok(onSortCompletion.calledWith({ sortedLines: '', error }));
     });
   });
